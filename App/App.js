e379eaf0d25e50162bf6f3f2cc288b1f798c15fa
William Peak <williamjpeak@gmail.com>
2023-11-28 20:58:31 -0500

    Simple movie list

--- App/App.js
+++ App/App.js
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import 'react-native-gesture-handler';
+// import React, { useEffect, useState } from 'react'
+// import { firebase } from './src/firebase/config'
+// import { NavigationContainer } from '@react-navigation/native'
+// import { createStackNavigator } from '@react-navigation/stack'
+// import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
+// import {decode, encode} from 'base-64'
+// if (!global.btoa) {  global.btoa = encode }
+// if (!global.atob) { global.atob = decode }
 
-export default function App() {
+// const Stack = createStackNavigator();
+
+// export default function App() {
+
+//   const [loading, setLoading] = useState(true)
+//   const [user, setUser] = useState(null)
+
+//   useEffect(() => {
+//     const usersRef = firebase.firestore().collection('users');
+//     firebase.auth().onAuthStateChanged(user => {
+//       if (user) {
+//         usersRef
+//           .doc(user.uid)
+//           .get()
+//           .then((document) => {
+//             const userData = document.data()
+//             setLoading(false)
+//             setUser(userData)
+//           })
+//           .catch((error) => {
+//             setLoading(false)
+//           });
+//       } else {
+//         setLoading(false)
+//       }
+//     });
+//   }, []);
+
+//   if (loading) {
+//     return (
+//       <></>
+//     )
+//   }
+
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         { user ? (
+//           <Stack.Screen name="Home">
+//             {props => <HomeScreen {...props} extraData={user} />}
+//           </Stack.Screen>
+//         ) : (
+//           <>
+//             <Stack.Screen name="Login" component={LoginScreen} />
+//             <Stack.Screen name="Registration" component={RegistrationScreen} />
+//           </>
+//         )}
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, FlatList, Text, View} from 'react-native';
+
+const App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const response = await fetch('https://reactnative.dev/movies.json');
+      const json = await response.json();
+      setData(json.movies);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, padding: 24, paddingTop: 100}}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({id}) => id}
+          renderItem={({item}) => (
+            <Text>
+              {item.title}, {item.releaseYear}
+            </Text>
+          )}
+        />
+      )}
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
